1dc8d2db1fcb16fcd5fa4e5b6c79da5e55171aa3
Samantha Alvarenga <86083664+Samantha1024a-l@users.noreply.github.com>
2024-07-31 19:54:34 -0300

    Add files via upload

---
+++ operacoes.js
@@ -0,0 +1,9 @@
+function operacoesBasicas(numeroA, numeroB) {
+    console.log(`${numeroA} + ${numeroB} = ${numeroA + numeroB}`);
+    console.log(`${numeroA} - ${numeroB} = ${numeroA - numeroB}`);
+    console.log(`${numeroA} x ${numeroB} = ${numeroA * numeroB}`);
+    console.log(`${numeroA} / ${numeroB} = ${numeroA / numeroB}`);
+}
+
+// Exemplo de uso
+operacoesBasicas(4, 5);
